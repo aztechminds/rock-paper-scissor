60769184361c1db86a1db656d1a5fd1cf15c43bf
techminds <aztechminds@gmail.com>
2025-01-15 14:52:10 +0100

    modified css and html

--- Rock-Paper-Scissor-Vite/eslint.config.js
+++ Rock-Paper-Scissor-Vite/eslint.config.js
@@ -8,3 +8,18 @@ export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
 ];
+
+// {
+//   rules: {
+//     "no-console": "off",
+//     "no-unused-vars": "off",
+//     "no-undef": "off",
+//     "prefer-const": "error",
+//     "eqeqeq": "error",
+//     "quotes": ["error", "double"],
+//     "semi": ["error", "always"],
+//     "no-var": "error",
+//     "curly": "error",
+//     "camelcase": "error",
+//   }
+// }
